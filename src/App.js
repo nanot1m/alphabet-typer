1f6d62ddf07488dfbeccfd0448511b22ea1fa60c
tk338g <tkhazamov@wayfair.com>
2021-08-03 20:50:58 +0200

    Init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,223 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState, memo } from "react";
+import { Input, Container, Button, Text, Box, Stack } from "@chakra-ui/react";
+import { FaTwitter } from "react-icons/fa";
+import { alphabetRu } from "./alphabets";
+
+const States = {
+  MainMenu: "main-menu",
+  Game: "game",
+  ScoreBoard: "score-board",
+};
+
+const initialState = {
+  screen: States.MainMenu,
+};
+
+let gameId = 0;
+const getGameId = () => gameId++;
 
 function App() {
+  const [gameState, setGameState] = useState(initialState);
+
+  switch (gameState.screen) {
+    case States.MainMenu:
+      return (
+        <MainMenuScreen
+          onStart={() =>
+            setGameState({
+              screen: States.Game,
+              gameId: getGameId(),
+            })
+          }
+        />
+      );
+    case States.Game:
+      return (
+        <GameScreen
+          key={gameId}
+          onRestart={() =>
+            setGameState({
+              screen: States.Game,
+              gameId: getGameId(),
+            })
+          }
+          onFinish={(score) =>
+            setGameState({ screen: States.ScoreBoard, score })
+          }
+        />
+      );
+    case States.ScoreBoard:
+      return (
+        <ScoreScreen
+          score={gameState.score}
+          onRestart={() =>
+            setGameState({
+              screen: States.Game,
+              gameId: getGameId(),
+            })
+          }
+        />
+      );
+    default:
+      throw new Error("Unknown game state:", gameState);
+  }
+}
+
+function MainMenuScreen({ onStart }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
+    <ScreenWrapper>
+      <Text mb={8} textAlign="center" fontSize="xl">
+        Узнай, как быстро ты сможешь набрать весь алфавит на клавиатуре
+      </Text>
+      <Button autoFocus onClick={onStart}>
+        Поехали!
+      </Button>
+    </ScreenWrapper>
+  );
+}
+
+const joinedAlphabet = alphabetRu.join("");
+
+function GameScreen({ onRestart, onFinish }) {
+  const [score, setScore] = useState(0);
+  const [inputValue, setInputValue] = useState("");
+  const [gameStarted, setGameStarted] = useState(false);
+  const [countDown, setCountDown] = useState(3);
+
+  useEffect(() => {
+    if (countDown === 0) {
+      setGameStarted(true);
+      return;
+    }
+    const interval = setTimeout(() => {
+      setCountDown((countDown) => countDown - 1);
+    }, 1000);
+    return () => clearTimeout(interval);
+  }, [countDown, gameStarted]);
+
+  useEffect(() => {
+    if (!gameStarted) {
+      return;
+    }
+
+    const startTime = Date.now();
+    const intervalId = setInterval(() => {
+      setScore(Date.now() - startTime);
+    }, 1);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [gameStarted]);
+
+  useEffect(() => {
+    if (inputValue.toLowerCase() === joinedAlphabet) {
+      onFinish(score);
+    }
+  }, [inputValue, onFinish, score]);
+
+  if (!gameStarted) {
+    return (
+      <ScreenWrapper>
+        <Text fontSize="9xl">{countDown}</Text>
+      </ScreenWrapper>
+    );
+  }
+
+  return (
+    <ScreenWrapper>
+      <Box mb={4} alignSelf="flex-end">
+        <Result score={score} />
+      </Box>
+      <Box mb={4}>
+        <Alphabet alphabet={alphabetRu} inputValue={inputValue} />
+      </Box>
+      <Input
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        mb={8}
+        autoFocus
+      />
+      <Button onClick={onRestart}>Попробовать снова</Button>
+    </ScreenWrapper>
+  );
+}
+
+function ScoreScreen({ score, onRestart }) {
+  return (
+    <ScreenWrapper>
+      <Text mb={8} textAlign="center" fontSize="xl" alignSelf>
+        <Result score={score} />
+      </Text>
+      <Stack>
+        <Button onClick={onRestart}>Попробовать снова</Button>
+        <Button
+          as="a"
+          href={
+            "https://twitter.com/intent/tweet?text=" +
+            "Я набрал весь алфавит на клавиатуре за " +
+            (score / 1000).toFixed(2) +
+            " сек."
+          }
+          aria-label="tweet"
+          rightIcon={<FaTwitter />}
+          colorScheme="blue"
           target="_blank"
-          rel="noopener noreferrer"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Поделиться
+        </Button>
+      </Stack>
+    </ScreenWrapper>
+  );
+}
+
+function ScreenWrapper({ children }) {
+  return (
+    <Container
+      h="full"
+      pb={16}
+      pt={8}
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+    >
+      {children}
+    </Container>
+  );
+}
+
+const Alphabet = memo(({ alphabet, inputValue }) => {
+  return (
+    <Box display="flex">
+      {alphabet.map((letter, i) => (
+        <Text
+          key={letter}
+          fontSize={["l", "xl", "x-large"]}
+          color={
+            inputValue[i] === undefined
+              ? "gray.400"
+              : inputValue[i]?.toLowerCase() === letter
+              ? "gray.800"
+              : "red"
+          }
+        >
+          {letter.toUpperCase()}
+        </Text>
+      ))}
+    </Box>
+  );
+});
+
+function Result({ score }) {
+  return (
+    <Box>
+      <Text textAlign="left" fontSize="xl">
+        Твой результат
+      </Text>
+      <Text fontSize="xx-large">{(score / 1000).toFixed(2)}</Text>
+      <Text fontSize="xl">сек.</Text>
+    </Box>
   );
 }
 
